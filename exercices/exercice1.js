9af93d1c9b2d7f45d5a10ba0cc8ca2e01408ef58
Dendeey <davidsim1035@gmail.com>
2023-01-03 09:56:32 +0000

    Exos

---
+++ exercices/exercice1.js
@@ -0,0 +1,21 @@
+/* Ex 1 */
+console.log("Hello world");
+
+/* Ex 2 */
+let color;
+color = "red";
+console.log(color);
+
+/* Ex 3 */
+const msg = "C'est une constante";
+console.log(msg);
+
+/* Ex 4 */
+let string = "String";
+console.log(string);
+
+/* Ex 5 */
+let x = 66;
+let y = 12;
+let result = x + y;
+console.log(result);
