49c1514c2b4771e3d622b422d2b65a6a2155af8b
Dendeey <davidsim1035@gmail.com>
2023-01-03 11:58:30 +0000

    Exos

--- exercices/exercice1.js
+++ exercices/exercice1.js
@@ -19,3 +19,20 @@ let x = 66;
 let y = 12;
 let result = x + y;
 console.log(result);
+
+/* Ex 6*/
+let userFirstName = window.prompt("Comment t'appelles-tu ?");
+console.log(`Salut ${userFirstName}`);
+
+/* Ex 7*/
+let userName = window.prompt("Quel est ton nom ?");
+let userAge = window.prompt("En quelle année es-tu né ?");
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let currentDate = `${year}-${month}-${day}`;
+let resultAge = currentDate - userAge;
+console.log(`Salut ${userFirstName} ${userName}, cette année tu ${resultAge} ans !`);
+
+
